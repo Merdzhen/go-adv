7cd3c468f2e9b8d876bde2bc49a10710c89f46fb
Merdzhen <merdzhen.k@gmail.com>
2025-12-22 20:59:55 +0300

    3.6.-3.7. Go allocator, пример с Reader

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
-func main() {
-	age := getAge()
-	canDrink(age)
 
-	// go run -gcflags '-m -l' main.go
+// https://pkg.go.dev/io#Reader
+type Reader interface {
+	Read(p []byte) (n int, err error)
 }
 
-func canDrink(age *int) bool {
-	return *age >= 18
-}
+// Зачем такая сигнатура:
+// Внешний буфер передается, чтобы управлять памятью эффективно.
+// Избегается создание внутреннего объекта, который позже нужно будет чистить, 
+// что связано с особенностями работы стека и кучи.
 
-func getAge() *int {
-	age := 18
-	return &age
+type Reader2 interface {
+	Read() (p []byte, err error)
+	//в этом случае p ушел бы на кучу
 }
