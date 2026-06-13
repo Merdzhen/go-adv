b422b3aa783f884d4457f4462461d6bd101c29be
Merdzhen <merdzhen.k@gmail.com>
2026-06-13 16:36:22 +0300

    4.3. goroutine

--- main.go
+++ main.go
@@ -1,17 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
-// https://pkg.go.dev/io#Reader
-type Reader interface {
-	Read(p []byte) (n int, err error)
+// написать функцию которая делает 10 конкурентных запросов на get по google.com
+// вывести в консоль 10 statuscode
+
+func main() {
+	t := time.Now()
+	for i := 0; i < 10; i++ {
+		go getHttpStatus()
+	}
+	time.Sleep(time.Millisecond * 1500)
+	fmt.Println(time.Since(t))
 }
 
-// Зачем такая сигнатура:
-// Внешний буфер передается, чтобы управлять памятью эффективно.
-// Избегается создание внутреннего объекта, который позже нужно будет чистить, 
-// что связано с особенностями работы стека и кучи.
-
-type Reader2 interface {
-	Read() (p []byte, err error)
-	//в этом случае p ушел бы на кучу
+func getHttpStatus() {
+	resp, err := http.Get("https://google.com")
+	if err != nil {
+		fmt.Printf("Ошибка %s", err.Error())
+		return
+	}
+	fmt.Printf("Код %d \n", resp.StatusCode)
 }
