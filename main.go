f7874a35cc3bf73014c36e8697c81fd215e216b8
Merdzhen <merdzhen.k@gmail.com>
2026-06-14 13:15:49 +0300

    4.8. Создание канала

--- main.go
+++ main.go
@@ -3,32 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"sync"
-	"time"
 )
 
-// написать функцию которая делает 10 конкурентных запросов на get по google.com
-// вывести в консоль 10 statuscode
 
 func main() {
-	t := time.Now()
-	var wg sync.WaitGroup
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go func() {
-			getHttpStatus()
-			wg.Done()
-		}()
-	}
-	wg.Wait()
-	fmt.Println(time.Since(t))
+	code := make(chan int)
+	go getHttpStatus(code)
+	<- code
 }
 
-func getHttpStatus() {
+func getHttpStatus(codeCh chan int) {
 	resp, err := http.Get("https://google.com")
 	if err != nil {
 		fmt.Printf("Ошибка %s", err.Error())
 		return
 	}
 	fmt.Printf("Код %d \n", resp.StatusCode)
+	codeCh <- resp.StatusCode
 }
