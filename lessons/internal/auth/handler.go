11a894fcb1cfa4d3cf1cf413ead8c04171b7edbb
Merdzhen <merdzhen.k@gmail.com>
2026-06-24 14:37:38 +0300

    7.3. Regexp / валидация email

--- lessons/internal/auth/handler.go
+++ lessons/internal/auth/handler.go
@@ -6,6 +6,7 @@ import (
 	"go/adv-demo/configs"
 	"go/adv-demo/pkg/res"
 	"net/http"
+	"net/mail"
 )
 
 type AuthHandlerDeps struct {
@@ -28,23 +29,43 @@ func (handler *AuthHandler) Login() http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		var payload LoginRequest
 		err := json.NewDecoder(req.Body).Decode(&payload)
-		
 		if err != nil {
 			res.Json(w, err.Error(), 400)
 			return
 		}
 
 		if payload.Email == "" {
-      res.Json(w, "Email required", 400)
-      return
-    }
+			res.Json(w, "Email required", 400)
+			return
+		}
+
+		// способ 1
+		// reg, _ := regexp.Compile(`[A-Za-z0-9\._%+\-]+@[A-Za-z0-9\.\-]+\.[A-Za-z]{2,}`)
+		// if !reg.MatchString(payload.Email) {
+		// 	res.Json(w, "Wrong email", 400)
+		//   return
+		// }
+
+		// способ 2
+		// match, _ := regexp.MatchString(`[A-Za-z0-9\._%+\-]+@[A-Za-z0-9\.\-]+\.[A-Za-z]{2,}`, payload.Email)
+		// if !match {
+		// 	res.Json(w, "Wrong email", 400)
+		//   return
+		// }
+
+		// способ 3
+		mailAddress, err := mail.ParseAddress(payload.Email)
+		if err != nil {
+			res.Json(w, "Wrong email", 400)
+			return
+		}
+		fmt.Println(mailAddress)
 
 		if payload.Password == "" {
-      res.Json(w, "Password required", 400)
-      return
-    }
+			res.Json(w, "Password required", 400)
+			return
+		}
 
-		fmt.Println(payload)
 		data := LoginResponse{
 			Token: "123",
 		}
