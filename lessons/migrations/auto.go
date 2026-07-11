c6c906fa666d4a81663dc40f82f7a11438c085be
Merdzhen <merdzhen.k@gmail.com>
2026-07-11 17:58:33 +0300

    11.2. Упражнение - Модель пользователя

--- lessons/migrations/auto.go
+++ lessons/migrations/auto.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"go/adv-demo/internal/link"
+	"go/adv-demo/internal/user"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -20,5 +21,5 @@ func main() {
 		panic(err)
 	}
 
-	db.AutoMigrate(&link.Link{})
+	db.AutoMigrate(&link.Link{}, &user.User{})
 }
