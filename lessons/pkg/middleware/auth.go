4a08dd2b459b54e6c457fbc6fe16abaade889905
Merdzhen <merdzhen.k@gmail.com>
2026-07-16 18:08:59 +0300

    12.6. Запись в контекст

--- lessons/pkg/middleware/auth.go
+++ lessons/pkg/middleware/auth.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"context"
 	"fmt"
 	"go/adv-demo/configs"
 	"go/adv-demo/pkg/jwt"
@@ -8,12 +9,23 @@ import (
 	"strings"
 )
 
+type key string
+
+const (
+	ContextEmailKey key = "ContextEmailKey"
+)
+
+
 func IsAuthed(next http.Handler, config *configs.Config) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		token := strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
 		isValid, data := jwt.NewJWT(config.Auth.Secret).Parse(token)
 		fmt.Println("isValid", isValid)
 		fmt.Println("data", data)
-		next.ServeHTTP(w, r)
+
+		ctx := context.WithValue(r.Context(), ContextEmailKey, data.Email)
+		req := r.WithContext(ctx)
+
+		next.ServeHTTP(w, req)
 	})
 }
