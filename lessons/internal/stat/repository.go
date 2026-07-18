e6f34aafd680bfc55e42b0d2b7c79077dc93cf08
Merdzhen <merdzhen.k@gmail.com>
2026-07-18 15:55:04 +0300

    13.7. Добавление клика

---
+++ lessons/internal/stat/repository.go
@@ -0,0 +1,35 @@
+package stat
+
+import (
+	"go/adv-demo/pkg/db"
+	"time"
+
+	"gorm.io/datatypes"
+)
+
+type StatRepository struct {
+	*db.Db
+}
+
+func NewStatRepository(db *db.Db) *StatRepository {
+	return &StatRepository{
+		Db: db,
+	}
+}
+
+func (repo *StatRepository) AddClick(linkId uint) {
+	// если нет статистики за сегодня по ссылке - создаем, если есть +1
+	var stat Stat
+	currentDate := datatypes.Date(time.Now())
+	repo.Db.Find(&stat, "link_id = ? and date = ?", linkId, currentDate)
+	if stat.ID == 0 {
+		repo.Db.Create(&Stat{
+			LinkId: linkId,
+			Clicks: 1,
+			Date:   currentDate,
+		})
+	} else {
+		stat.Clicks += 1
+		repo.DB.Save(&stat)
+	}
+}
