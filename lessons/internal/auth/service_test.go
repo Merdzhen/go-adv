f7e97b5629d257e7830ccacf6e8b352dc7cb0e52
Merdzhen <merdzhen.k@gmail.com>
2026-08-12 13:45:09 +0300

    14.9. Mock данных (unit тест с моком)

---
+++ lessons/internal/auth/service_test.go
@@ -0,0 +1,29 @@
+package auth_test
+
+import (
+	"go/adv-demo/internal/auth"
+	"go/adv-demo/internal/user"
+	"testing"
+)
+
+type MockUserRepository struct {}
+
+func (repo *MockUserRepository) Create(*user.User) (*user.User, error) {
+	return nil, nil
+}
+
+func (repo *MockUserRepository) FindByEmail(email string) (*user.User, error) {
+	return nil, nil
+}
+
+func TestRegisterSuccess(t *testing.T) {
+	const initialEmail = "a@a.ru"
+	authservice := auth.NewAuthService(&MockUserRepository{})
+	email, err := authservice.Register(initialEmail, "1", "Vasiliy")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if email != initialEmail {
+		t.Fatalf("Email %s do not match %s", email, initialEmail)
+	}
+}
